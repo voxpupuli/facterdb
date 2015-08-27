a905f6f226722af35627a14b833c78602531b428
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-08-27 11:43:58 +0200

    Fix specs

--- spec/facterdb_spec.rb
+++ spec/facterdb_spec.rb
@@ -3,9 +3,8 @@ require 'spec_helper'
 describe 'FacterDB' do
 
   describe '#get_os_facts' do
-
     context 'without parameters' do
-      subject { get_os_facts() }
+      subject { FacterDB.get_os_facts() }
       it "Should return an array of hashes with at least 1 element" do
         expect(subject.class).to eq Array
         expect(subject.size).not_to eq(0)
