2c311915954594518550cf1bb83fc8fb196f3a6f
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-08-27 13:15:22 +0200

    Use a relative factsdir for the lib

--- lib/facterdb.rb
+++ lib/facterdb.rb
@@ -4,9 +4,10 @@ require 'jgrep'
 module FacterDB
 
   def self.get_os_facts(facter_version='*', filter=[])
+    facts_dir = File.expand_path(File.join(File.dirname(__FILE__), '../facts'))
     filter_str = filter.map { |f| f.map { |k,v | "#{k}=#{v}" }.join(' and ') }.join(' or ')
 
-    jsons = Dir.glob("facts/#{facter_version}/*.facts").map { |f| File.read(f) }
+    jsons = Dir.glob("#{facts_dir}/#{facter_version}/*.facts").map { |f| File.read(f) }
     json = "[#{jsons.join(',')}]\n"
     JGrep.jgrep(json, filter_str)
   end
