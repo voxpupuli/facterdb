dc3cfc3b4402a428099b7cfe00b441363bb523c8
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-08-30 09:54:07 +0200

    Filter can also be a Hash or a String

--- lib/facterdb.rb
+++ lib/facterdb.rb
@@ -4,8 +4,17 @@ require 'jgrep'
 module FacterDB
 
   def self.get_os_facts(facter_version='*', filter=[])
+    if filter.is_a?(Array)
+      filter_str = filter.map { |f| f.map { |k,v | "#{k}=#{v}" }.join(' and ') }.join(' or ')
+    elsif filter.is_a?(Hash)
+      filter_str = filter.map { |k,v | "#{k}=#{v}" }.join(' and ')
+    elsif filter.is_a?(String)
+      filter_str = filter
+    else
+      raise 'filter must be either an Array a Hash or a String'
+    end
+
     facts_dir = File.expand_path(File.join(File.dirname(__FILE__), '../facts'))
-    filter_str = filter.map { |f| f.map { |k,v | "#{k}=#{v}" }.join(' and ') }.join(' or ')
 
     jsons = Dir.glob("#{facts_dir}/#{facter_version}/*.facts").map { |f| File.read(f) }
     json = "[#{jsons.join(',')}]\n"
