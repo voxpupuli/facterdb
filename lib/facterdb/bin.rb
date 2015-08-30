3c9b5bb3a374290fe3258619b90c5e6665f020ab
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-08-30 09:54:45 +0200

    Add CLI

---
+++ lib/facterdb/bin.rb
@@ -0,0 +1,14 @@
+require 'facterdb'
+require 'json'
+
+module FacterDB
+  class Bin
+    def initialize(args)
+      @args = args
+    end
+
+    def run
+      puts JSON.pretty_generate(FacterDB::get_os_facts('*', @args[0]))
+    end
+  end
+end
