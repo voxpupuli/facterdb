86cad7dd75b103102470a499641252f24c09e5c5
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-08-31 10:23:03 +0200

    Deprecate get_os_facts

--- lib/facterdb.rb
+++ lib/facterdb.rb
@@ -3,7 +3,39 @@ require 'jgrep'
 
 module FacterDB
 
+  def self.database
+    @database ||= "[#{Dir.glob("#{File.expand_path(File.join(File.dirname(__FILE__), '../facts'))}/*/*.facts").map { |f| File.read(f) }.join(',')}]\n"
+  end
+
   def self.get_os_facts(facter_version='*', filter=[])
+    if facter_version == '*'
+      if filter.is_a?(Array)
+        filter_str = filter.map { |f| f.map { |k,v | "#{k}=#{v}" }.join(' and ') }.join(' or ')
+      elsif filter.is_a?(Hash)
+        filter_str = filter.map { |k,v | "#{k}=#{v}" }.join(' and ')
+      elsif filter.is_a?(String)
+        filter_str = filter
+      else
+        raise 'filter must be either an Array a Hash or a String'
+      end
+    else
+      if filter.is_a?(Array)
+        filter_str = "facterversion=/^#{facter_version}/ and (#{filter.map { |f| f.map { |k,v | "#{k}=#{v}" }.join(' and ') }.join(' or ')})"
+      elsif filter.is_a?(Hash)
+        filter_str = "facterversion=/^#{facter_version}/ and (#{filter.map { |k,v | "#{k}=#{v}" }.join(' and ')})"
+      elsif filter.is_a?(String)
+        filter_str = "facterversion=/^#{facter_version}/ and (#{filter})"
+      else
+        raise 'filter must be either an Array a Hash or a String'
+      end
+    end
+
+    warn "[DEPRECATION] `get_os_facts` is deprecated. Please use `get_facts(#{filter_str})` instead."
+
+    get_facts(filter_str)
+  end
+
+  def self.get_facts(filter=[])
     if filter.is_a?(Array)
       filter_str = filter.map { |f| f.map { |k,v | "#{k}=#{v}" }.join(' and ') }.join(' or ')
     elsif filter.is_a?(Hash)
@@ -13,11 +45,6 @@ module FacterDB
     else
       raise 'filter must be either an Array a Hash or a String'
     end
-
-    facts_dir = File.expand_path(File.join(File.dirname(__FILE__), '../facts'))
-
-    jsons = Dir.glob("#{facts_dir}/#{facter_version}/*.facts").map { |f| File.read(f) }
-    json = "[#{jsons.join(',')}]\n"
-    JGrep.jgrep(json, filter_str).map { |hash| Hash[hash.map{ |k, v| [k.to_sym, v] }] }
+    JGrep.jgrep(database, filter_str).map { |hash| Hash[hash.map{ |k, v| [k.to_sym, v] }] }
   end
 end
--- spec/facterdb_spec.rb
+++ spec/facterdb_spec.rb
@@ -48,4 +48,42 @@ describe 'FacterDB' do
       end
     end
   end
+
+  describe '#get_facts' do
+    context 'without parameters' do
+      subject { FacterDB.get_facts() }
+      it "Should return an array of hashes with at least 1 element" do
+        expect(subject.class).to eq Array
+        expect(subject.size).not_to eq(0)
+        expect(subject.select { |facts| facts.class != Hash }.size).to eq(0)
+      end
+    end
+
+    context 'with an Array filter' do
+      subject { FacterDB.get_facts([{:osfamily => 'Debian'}]) }
+      it "Should return an array of hashes with at least 1 element" do
+        expect(subject.class).to eq Array
+        expect(subject.size).not_to eq(0)
+        expect(subject.select { |facts| facts.class != Hash }.size).to eq(0)
+      end
+    end
+
+    context 'with a Hash filter' do
+      subject { FacterDB.get_facts({:osfamily => 'Debian'}) }
+      it "Should return an array of hashes with at least 1 element" do
+        expect(subject.class).to eq Array
+        expect(subject.size).not_to eq(0)
+        expect(subject.select { |facts| facts.class != Hash }.size).to eq(0)
+      end
+    end
+
+    context 'with a String filter' do
+      subject { FacterDB.get_facts('osfamily=Debian') }
+      it "Should return an array of hashes with at least 1 element" do
+        expect(subject.class).to eq Array
+        expect(subject.size).not_to eq(0)
+        expect(subject.select { |facts| facts.class != Hash }.size).to eq(0)
+      end
+    end
+  end
 end
