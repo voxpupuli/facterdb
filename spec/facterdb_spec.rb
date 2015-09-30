773ab2d11ed6550d47df2bbfa75f95b8aa5c0224
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-09-30 14:29:34 +0200

    Add unit tests

--- spec/facterdb_spec.rb
+++ spec/facterdb_spec.rb
@@ -11,5 +11,41 @@ describe 'FacterDB' do
         expect(subject.select { |facts| facts.class != Hash }.size).to eq(0)
       end
     end
+
+    context 'without parameters' do
+      subject { FacterDB.get_os_facts() }
+      it "Should return an array of hashes with at least 1 element" do
+        expect(subject.class).to eq Array
+        expect(subject.size).not_to eq(0)
+        expect(subject.select { |facts| facts.class != Hash }.size).to eq(0)
+      end
+    end
+
+    context 'with an Array filter' do
+      subject { FacterDB.get_os_facts('*', [{:osfamily => 'Debian'}]) }
+      it "Should return an array of hashes with at least 1 element" do
+        expect(subject.class).to eq Array
+        expect(subject.size).not_to eq(0)
+        expect(subject.select { |facts| facts.class != Hash }.size).to eq(0)
+      end
+    end
+
+    context 'with a Hash filter' do
+      subject { FacterDB.get_os_facts('*', {:osfamily => 'Debian'}) }
+      it "Should return an array of hashes with at least 1 element" do
+        expect(subject.class).to eq Array
+        expect(subject.size).not_to eq(0)
+        expect(subject.select { |facts| facts.class != Hash }.size).to eq(0)
+      end
+    end
+
+    context 'with a String filter' do
+      subject { FacterDB.get_os_facts('*', 'osfamily=Debian') }
+      it "Should return an array of hashes with at least 1 element" do
+        expect(subject.class).to eq Array
+        expect(subject.size).not_to eq(0)
+        expect(subject.select { |facts| facts.class != Hash }.size).to eq(0)
+      end
+    end
   end
 end
