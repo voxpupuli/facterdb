9f649c66c71c5102306e9920319047301e77b8fa
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-03-26 14:06:36 +0800

    (#77) Add duplicate factset file test
    Previously there were many default fact files which are just duplicates.  This
    commit adds a test will performs a simple file hash comparison of all default
    factset files and fails and displays any duplicate factsets.
    
    This test is initially skipped while the duplicate files are removed in later
    commits.

---
+++ spec/facts_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'digest'
+
+RSpec::Matchers.define :have_a_unique_hash do |expected|
+  match do |actual|
+    actual.count == 1
+  end
+
+  failure_message do |actual|
+    "expected that fact files #{actual.join(', ')} would be unique"
+  end
+end
+
+describe 'Default Facts' do
+  before(:each) do
+    ENV['FACTERDB_SKIP_DEFAULTDB'] = nil
+    FacterDB.instance_variable_set(:@database, nil)
+  end
+
+  describe 'fact files' do
+    xit 'should not contain duplicate fact sets' do
+      # Gather all of the default files and hash the content
+      file_hashes = {}
+      FacterDB.default_fact_files.each do |filepath|
+        file_hash = Digest::SHA256.hexdigest( File.open(filepath, 'rb') { |file| file.read } )
+        file_hashes[file_hash] ||= []
+        file_hashes[file_hash] << filepath
+      end
+
+      file_hashes.keys.each do |file_hash|
+        expect(file_hashes[file_hash]).to have_a_unique_hash
+      end
+    end
+  end
+end
