d85f3074f004a6a553b3b3abf804a2ffb9bd7520
Tim Sharpe <tim@bombasticmonkey.com>
2019-07-20 20:03:22 +1000

    Improve coverage of FacterDB.external_fact_files

--- spec/facterdb_spec.rb
+++ spec/facterdb_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'FacterDB' do
+describe FacterDB do
   shared_examples 'returns a result' do
     it 'returns an Array' do
       expect(result).to be_an_instance_of(Array)
@@ -15,44 +15,108 @@ describe 'FacterDB' do
     end
   end
 
-  describe 'database' do
-    let(:facts_24_path)  { File.join(project_dir, 'facts', '2.4') }
-    let(:facts_24_count) { Dir.glob(File.join(facts_24_path,'*.facts')).count }
-    let(:facts_23_path)  { File.join(project_dir, 'facts', '2.3') }
-    let(:facts_23_count) { Dir.glob(File.join(facts_23_path,'*.facts')).count }
+  let(:facts_24_path)  { File.join(project_dir, 'facts', '2.4') }
+  let(:facts_24_count) { Dir.glob(File.join(facts_24_path,'*.facts')).count }
+  let(:facts_23_path)  { File.join(project_dir, 'facts', '2.3') }
+  let(:facts_23_count) { Dir.glob(File.join(facts_23_path,'*.facts')).count }
 
+  describe '.use_defaultdb?' do
+    subject { described_class.use_defaultdb? }
+
+    before(:each) do
+      ENV.delete('FACTERDB_SKIP_DEFAULTDB')
+    end
+
+    context 'when FACTERDB_SKIP_DEFAULTDB environment variable is unset' do
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when FACTERDB_SKIP_DEFAULTDB environment variable is set' do
+      before(:each) do
+        ENV['FACTERDB_SKIP_DEFAULTDB'] = '1'
+      end
+
+      it { is_expected.to be_falsey }
+    end
+  end
+
+  describe '.external_fact_files' do
+    subject(:file_count) { described_class.external_fact_files.count }
+
+    let(:non_directory_path) { File.join('this', 'is', 'not', 'a', 'directory') }
+
+    before(:each) do
+      ENV.delete('FACTERDB_SEARCH_PATHS')
+      allow(File).to receive(:directory?).and_call_original
+      allow(File).to receive(:directory?).with(non_directory_path).and_return(false)
+    end
+
+    context 'without an argument or FACTERDB_SEARCH_PATHS environment variable set' do
+      it { is_expected.to be_zero }
+    end
+
+    context 'when the FACTERDB_SEARCH_PATHS environment variable is set' do
+      it 'supports a single path' do
+        ENV['FACTERDB_SEARCH_PATHS'] = facts_24_path
+
+        expect(file_count).to eq(facts_24_count)
+      end
+
+      it 'supports multiple paths delimited by the OS path separator' do
+        ENV['FACTERDB_SEARCH_PATHS'] = [facts_24_path, facts_23_path].join(File::PATH_SEPARATOR)
+
+        expect(file_count).to eq(facts_24_count + facts_23_count)
+      end
+
+      it 'ignores paths that are not a directory' do
+        ENV['FACTERDB_SEARCH_PATHS'] = [facts_24_path, non_directory_path].join(File::PATH_SEPARATOR)
+
+        object = defined?(Warning) ? Warning : Kernel
+        allow(object).to receive(:warn).and_call_original
+        allow(object).to receive(:warn).with(a_string_matching(%r{is not a directory}))
+
+        expect(file_count).to eq(facts_24_count)
+      end
+    end
+
+    # TODO: There is no parameter validation in this method
+    context 'when passed an argument' do
+      subject(:file_count) { described_class.external_fact_files(fact_paths).count }
+
+      context 'that is a single path' do
+        let(:fact_paths) { facts_24_path }
+
+        it 'returns the paths to the fact sets in the specified path' do
+          expect(file_count).to eq(facts_24_count)
+        end
+      end
+
+      context 'that is multiple paths delimited by the OS path separator' do
+        let(:fact_paths) { [facts_24_path, facts_23_path].join(File::PATH_SEPARATOR) }
+
+        it 'returns the paths to the fact sets in all the specified paths' do
+          expect(file_count).to eq(facts_24_count + facts_23_count)
+        end
+      end
+
+      context 'that contains a path that is not a directory' do
+        let(:fact_paths) { [facts_24_path, non_directory_path].join(File::PATH_SEPARATOR) }
+
+        it 'ignores the path that is not a directory' do
+          object = defined?(Warning) ? Warning : Kernel
+          allow(object).to receive(:warn).and_call_original
+          allow(object).to receive(:warn).with(a_string_matching(%r{is not a directory}))
+
+          expect(file_count).to eq(facts_24_count)
+        end
+      end
+    end
+  end
+
+  describe 'database' do
     before(:each) do
       ENV['FACTERDB_SKIP_DEFAULTDB'] = nil
       FacterDB.instance_variable_set(:@database, nil)
-    end
-
-    it 'skip default facts' do
-      ENV['FACTERDB_SKIP_DEFAULTDB'] = 'true'
-      expect(FacterDB.use_defaultdb?).to eq(false)
-    end
-
-    it 'use default facts' do
-      expect(FacterDB.use_defaultdb?).to eq(true)
-    end
-
-    it '#external_fact_files with env variable' do
-      ENV['FACTERDB_SEARCH_PATHS'] = facts_24_path
-      expect(FacterDB.external_fact_files.count).to eq(facts_24_count)
-    end
-
-    it '#external_fact_files with env variable and multiple paths' do
-      ENV['FACTERDB_SEARCH_PATHS'] = [facts_24_path, facts_23_path].join(File::PATH_SEPARATOR)
-      expect(FacterDB.external_fact_files.count).to eq(facts_24_count + facts_23_count)
-    end
-
-    it '#external_fact_files with argument' do
-      ENV['FACTERDB_SEARCH_PATHS'] = nil
-      expect(FacterDB.external_fact_files(facts_24_path).count).to eq(facts_24_count)
-    end
-
-    it '#external_fact_files without argument or env variable' do
-      ENV['FACTERDB_SEARCH_PATHS'] = nil
-      expect(FacterDB.external_fact_files.count).to be 0
     end
 
     it '#default_fact_files' do
